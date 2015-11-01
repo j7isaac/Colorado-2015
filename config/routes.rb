d2a62efe216c165b91e27cb1d0d6078778cd8163
vagrant <vagrant@precise32.(none)>
2015-11-01 08:34:21 +0000

    Finished project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Quiz1::Application.routes.draw do
+  get "/pages/:page" => "pages#show"
+  root "pages#show", page: "index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
