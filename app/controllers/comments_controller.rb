d4bbc1f97e839d2691bd4c322bec6d6385f0bfd9
Jaime M <j7isaac@gmail.com>
2015-11-30 06:40:43 +0000

    Made the submit button create a new quote

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,4 +6,15 @@ class CommentsController < ApplicationController
 	def new
 		@comment = Comment.new
 	end
+
+	def create
+		Comment.create(comment_params)
+		redirect_to root_path
+	end
+
+	private
+
+	def comment_params
+		params.require(:comment).permit(:message)
+	end
 end
